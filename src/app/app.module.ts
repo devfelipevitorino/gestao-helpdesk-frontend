bd793083ae142fbb08b390dcbe1bd9121a8aaae0
devfelipevitorino <devfelipevitorino@gmail.com>
2025-02-19 09:33:17 -0300

    tecnico list componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,13 +26,15 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './componets/nav/nav.component';
 import { HomeComponent } from './componets/home/home.component';
 import { HeaderComponent } from './componets/header/header.component';
+import { TecnicoListComponent } from './componets/tecnico/tecnico-list/tecnico-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    TecnicoListComponent
   ],
   imports: [
     BrowserModule,
