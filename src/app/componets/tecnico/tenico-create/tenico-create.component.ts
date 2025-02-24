17f52a1425d4037945e777b0b33dc6a41b2cb38c
devfelipevitorino <devfelipevitorino@gmail.com>
2025-02-23 23:12:55 -0300

    mudanças visuais no cadastro de tecnico

--- src/app/componets/tecnico/tenico-create/tenico-create.component.ts
+++ src/app/componets/tecnico/tenico-create/tenico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, MinLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tenico-create',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tenico-create.component.css']
 })
 export class TenicoCreateComponent implements OnInit {
-
+  nome: FormControl = new FormControl(null, Validators.required);
+  cpf: FormControl = new FormControl(null, [Validators.minLength(11), Validators.required]);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, [Validators.minLength(6), Validators.required]);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean{
+    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
+  }
 }
