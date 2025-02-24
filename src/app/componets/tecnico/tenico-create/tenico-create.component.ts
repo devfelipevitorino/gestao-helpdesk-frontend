adefbb107a8e3645484114c53a2dc5f1394fcdea
devfelipevitorino <devfelipevitorino@gmail.com>
2025-02-24 09:40:49 -0300

    fazendo a requisicao para criacao de tecnico

--- src/app/componets/tecnico/tenico-create/tenico-create.component.ts
+++ src/app/componets/tecnico/tenico-create/tenico-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, MinLengthValidator, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tenico-create',
@@ -7,16 +10,47 @@ import { FormControl, MinLengthValidator, Validators } from '@angular/forms';
   styleUrls: ['./tenico-create.component.css']
 })
 export class TenicoCreateComponent implements OnInit {
+
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.required);
   cpf: FormControl = new FormControl(null, [Validators.minLength(11), Validators.required]);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, [Validators.minLength(6), Validators.required]);
-  constructor() { }
+  
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  create(): void{
+    this.service.create(this.tecnico).subscribe(() =>{
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+    }, ex => {
+      console.log(ex);
+    });
+  }
+
+  addPerfil(perfil: any): void{
+    if(this.tecnico.perfis.includes(perfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    }else{
+      this.tecnico.perfis.push(perfil);
+    }
   }
 
   validaCampos(): boolean{
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
   }
+
 }
