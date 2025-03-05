61a7d1d5dfb5a63e59641d5cedcd1ceb227db3f0
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-05 09:53:56 -0300

    servico para chamados

---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Chamado } from '../models/chamado';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(private http:HttpClient) { }
+
+  findById(id: any): Observable<Chamado>{
+      return this.http.get<Chamado>(`${API_CONFIG.baseUrl}/chamados/${id}`);
+  }
+
+  findAll(): Observable<Chamado[]>{
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+
+  create(cliente: Chamado): Observable<Chamado>{
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, cliente);
+  }
+
+  update(cliente: Chamado): Observable<Chamado>{
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${cliente.id}`, cliente);
+  }
+}
