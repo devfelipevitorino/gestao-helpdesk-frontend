27e80de2000e7e9b2c2aa012864a8f9758b73aa6
devfelipevitorino <devfelipevitorino@gmail.com>
2025-03-06 22:46:58 -0300

    criando rota de criacao de chamado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteCreateComponent } from './componets/cliente/cliente-create/clien
 import { ClienteUpdateComponent } from './componets/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './componets/cliente/cliente-delete/cliente-delete.component';
 import { ChamadoListComponent } from './componets/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './componets/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent },
@@ -31,6 +32,7 @@ const routes: Routes = [
       { path: "clientes/delete/:id", component: ClienteDeleteComponent },
 
       { path: "chamados", component: ChamadoListComponent },
+      { path: "chamados/create", component: ChamadoCreateComponent },
     ]
   }
 ];
